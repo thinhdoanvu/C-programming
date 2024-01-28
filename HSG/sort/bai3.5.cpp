e84ebc3f99cf08a8fab696171784479d474bb49e
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-01-28 07:50:42 +0700

    Add files via upload

--- HSG/sort/bai3.5.cpp
+++ HSG/sort/bai3.5.cpp
@@ -1,7 +1,9 @@
 //liet ke tat ca phan so trong khoang tu 0-1 cua N
 //sap xep theo thu tu tang dan, dua ra phan so thu k
-//F(5)=0/1 1/5 1/4 1/3 1/2 3/5 2/3 3/4 4/5 1/1
+//F(5)=0/1 1/5 1/4 1/3 2/5 1/2 3/5 2/3 3/4 4/5 1/1
+//k=5: 2/5 k=6:1/2 k=7:3/5
 //luu y: 2 phan so bang nhau thi chi duoc in 1 lan
+
 
 #include<bits/stdc++.h>
 //khai bao bien
@@ -9,6 +11,8 @@ int N;
 int p[100];
 int q[100];
 int dem=0;
+//de giam do phuc tap cua thuat toan, su dung mang danh dau de loai bo phan tu trung lap
+int stat[100]={0};
 
 void inps(){
     for(int i=0; i<dem; i++){
@@ -54,9 +58,42 @@ void sortps(int n){
     inps();
 }
 
+void removeps(int n){
+    cout<<"phan so sau khi loai bo trung lap"<<endl;
+    for(int i=0; i<dem-1; i++){
+        if(1.0*p[i]/q[i] == 1.0*p[i+1]/q[i+1]){
+            stat[i]=1;
+        }
+    }
+    
+    //in ra day khong trung lap
+    for(int i=0; i<dem; i++){
+        if(stat[i]==0){
+            cout<<p[i]<<"/"<<q[i]<<endl;
+        }
+    }
+}
+
+void psthu(int k, int n){
+    int count=0;
+    int i;
+    for(i=0; i<dem; i++){
+        if(stat[i]==0 && count<k){
+            count++;
+        }
+        if(count==k){
+            break;
+        }
+    }
+    //vi tri tim thay so k tai i
+    cout<<"phan so thu "<<k<<" la: "<<p[i]<<"/"<<q[i];
+}
+
 void F(int n){
     lapps(n);
     sortps(n);
+    removeps(n);
+    psthu(7,n);
 }
 
 //chuong trinh chinh
