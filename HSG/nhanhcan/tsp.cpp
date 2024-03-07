6c4442ab0b9ef30d1085a44905511ff29c7dcb67
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-03-07 22:58:36 +0700

    Update tsp.cpp

--- HSG/nhanhcan/tsp.cpp
+++ HSG/nhanhcan/tsp.cpp
@@ -1,20 +1,32 @@
+//cho truoc n thanh pho va 1 dinh xuat phat
+//tim tat ca phuong an de co the xuat phat tu 1 thanh pho va quay tro ve thanh pho ban dau
+//sao cho chi phi la be nhat
 #include <bits/stdc++.h>
 
 #define MAX 20 // Maximum number of vertices
 #define INPUT "tsp.inp"
 #define start 0
+#define MAX 20
+
 using namespace std;
 
 FILE *fp;
 int n;
 int C[MAX][MAX];
-int x[MAX];//thu tu duong di
-int CLOSE[MAX];//danh sach cac dinh da duyet
-int sum = 0;//tong chi phi
-int best = INT_MAX;//chi phi tot nhat
-int bestsolution[MAX];//duong di tot nhat
-//dem so lan thuc hien de so sanh co/ko co dieu kien cat tia
-int dem=0;
+int CLOSE[MAX];//mag danh dau cac dinh da duyet
+int x[MAX];//mang luu tru duong di
+
+//danh cho nhanh can
+int bestsolution[MAX];
+int sum=0;//chi phi hien tai cua duong di start->hientai
+int best=INT_MAX;//chi phi tot nha = vo cung lon
+
+//cac ham con
+void copymang(int src[], int des[], int size){
+    for(int i=0; i<size; i++){
+        des[i]=src[i];
+    }
+}
 
 void init(int a[], int size, int value) {
     for (int i = 0; i < size; i++) {
@@ -48,88 +60,41 @@ void readfile() {
 }
 
 void induongdi(){
-    //dieu kien la phai co duong di tu dinh cuoi cung - xuat phat
-    if(C[x[n-1]][start] > 0){
-        for (int i = 0; i < n; i++) {
-            cout << x[i] << " -> ";
-        }
-        cout << start << " - sum: "<<sum + C[x[n-1]][start]<< endl;
-        //sum: tong chi phi da di tu start -> n-1 + n-1 -> start
+    for(int i=0; i<n; i++){
+        cout<<x[i]<<"-> ";
+    }
+    //in dinh quay tro ve
+    cout<<start;
+}
+
+void update(){
+    if(sum+C[x[n-1]][start] < best){
+        best = sum+C[x[n-1]][start];
+        copymang(x,bestsolution,n);
     }
 }
 
-void induongdi_best(){
-    //dieu kien la phai co duong di tu dinh cuoi cung - xuat phat
-    //if(C[bestsolution[n-1]][start] > 0){
-        for (int i = 0; i < n; i++) {
-            cout << bestsolution[i] << " -> ";
-        }
-        cout << start << " - best: "<<best << endl;
-        //sum: tong chi phi da di tu start -> n-1 + n-1 -> start
-    //}
-}
-
-void update() {
-    if(sum + C[x[n-1]][start] < best){
-        best = sum + C[x[n-1]][start];
-        for(int i=0; i<n; i++){
-            bestsolution[i]=x[i];//
-        }
-    }
-}
-
-
-void nhanhcan(int idx){
-    //di tim cac dinh tiep theo sau dinh xuat phat
-    //dieu kien nhanh can
-    if(sum > best){
-        return;
-    }
-    dem++;
-    for (int i = 0; i < n; i++) {//duyet qua cac dinh ke tiep
-        if (CLOSE[i] == 0 && C[x[idx-1]][i]>0){
-            //dinh chua duyet CLOSE = 0
-            //C[x[i-1]][i]>0: co duong di tu dinh hien tai voi dinh truoc do            
-            x[idx] = i;
-            CLOSE[i] = 1; //dinh i da duyet
-            sum+=C[x[idx-1]][i];//tinh chi phi
-            //dieu kien de duyet het cac dinh
-            if (idx == n - 1) {//dieu kien toat quay lui
-                //induongdi();
-                update();
-            }
-            else{
-                nhanhcan(idx+1);//duyet qua dinh ke tiep
-            }
-            CLOSE[i] = 0; //reset lai dinh da duyet
-            sum-=C[x[idx-1]][i];//bo chi phi da tinh khi quay tro lai
-        }
-    }
-}
-
-void nhanhcan2(int idx){
-    //di tim cac dinh tiep theo sau dinh xuat phat
-    if (idx == n - 1){//neu duyet den dinh cuoi cung
-        //induongdi();
-        update();
+void thu(int idx){
+    if(idx==n-1 && C[x[n-1]][start]>0){//so dinh duyet = n-1: duyet het tat ca cac dinh va phai co chu trinh quay ve dinh xuat phat
+    //x[n-1]: dinh cuoi cung duoc duyet - luu trong mang x
+        induongdi();
+        cout<<" - sum: "<<sum+C[x[n-1]][start]<<endl;//in ra tong cua chi phi start - dinh ket thuc + quay ve
+        update();//cap nhat gia tri cho best va duong di best
         return;
     }
     //dieu kien cat tia
     if(sum > best){
         return;
     }
-    dem++;
-    //tim cac nghiem thanh phan
-    for (int i = 0; i < n; i++) {//duyet qua cac dinh ke tiep
-        if (CLOSE[i] == 0 && C[x[idx-1]][i]>0){
-            //dinh chua duyet CLOSE = 0
-            //C[x[i-1]][i]>0: co duong di tu dinh hien tai voi dinh truoc do            
-            x[idx] = i;
-            CLOSE[i] = 1; //dinh i da duyet
-            sum+=C[x[idx-1]][i];//tinh chi phi
-            nhanhcan(idx+1);//duyet qua dinh ke tiep  
-            CLOSE[i] = 0; //reset lai dinh da duyet
-            sum-=C[x[idx-1]][i];//bo chi phi da tinh khi quay tro lai
+    //tim cac nghiem con
+    for(int i=0; i<n; i++){
+        if(CLOSE[i]==0 && C[x[idx]][i]>0){//chua duyet va co duong di
+            sum+=C[x[idx]][i];//tong chi phi tu start - idx - i
+            x[idx+1]=i;//phan tu ke tiep cua idx = dinh da duyet
+            CLOSE[i]=1;//danh dau dinh da duyet
+            thu(idx+1);//quay lai dinh lien truoc
+            CLOSE[i]=0;//coi nhu dinh vua quay lai la chua duyet
+            sum-=C[x[idx]][i];//tong chi phi tu start - idx - i: tra lai do quay tro lai dinh truoc
         }
     }
 }
@@ -138,14 +103,19 @@ int main() {
     readfile();
     init(CLOSE, n, 0);//cac tp chua duyet
     init(x, n, -1);//duong di chua co - 1
-    init(bestsolution,n,INT_MAX);
-    CLOSE[start]=1;//dinh start da duyet (0)
-    x[0]=start;//dinh xuat phat (0) duoc xuyet
-    // nhanhcan2(1); //duyet tu dinh tiep theo = 1
-    nhanhcan(1); //duyet tu dinh tiep theo = 1
-    induongdi_best();//chi in ra 1 lan duy nhat khi tim duoc tot nhat
-    cout<<"so lan thuc hien: "<<dem<<endl;
-    //khi co dieu kien: 38
-    //khi ko co dieu kien: 41
+    init(bestsolution,n,-1);//khoi tao mang bestsolution
+
+    //bat dau tu dinh xuat phat
+    x[0]=start;//danh dau dinh dau tien duyet = start
+    CLOSE[start]=1;//danh dau dinh start da duyet
+    //lan thu dau tien
+    thu(0);
+    //in ra ket qua tot nhat
+    cout<<"duong di tot nhat: "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<bestsolution[i]<<"-> ";
+    }
+    //in dinh quay tro ve
+    cout<<start<<" - best: "<<best;
     return 0;
 }
